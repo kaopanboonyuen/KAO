6f4c287c109b4f399f99bf2a85cf0038328606cc
Kao Panboonyuen <panboonyuen.kao@gmail.com>
2026-06-02 13:19:59 +0700

    🧼 feat: create datasets/__init__.py

---
+++ datasets/__init__.py
@@ -0,0 +1,8 @@
+from .satellite import (
+    SatelliteInpaintingDataset,
+    MassachusettsRoadsDataset,
+    DeepGlobeDataset,
+    build_dataloader,
+    generate_random_mask,
+    generate_irregular_mask,
+)
